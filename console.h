626fb0a372f673c2e3fdf6bb5ed5af14b5a10f75
Vlad Shurupov <vlad.shurupov@gmail.com>
2019-09-16 10:45:33 +1000

    Separated out console.c and cpu.c

--- console.h
+++ console.h
@@ -2,19 +2,13 @@
 #define CONSOLE_H
 
 #include <stdio.h>
+#include "globals.h"
 
-/* see main.c for definition */
+unsigned char _getch(void);
 
-unsigned char _getch(void) {
-	return getchar();
-}
+void _putch(uint8 ch);
 
-void _putch(uint8 ch) {
-	putchar(ch);
-}
-
-
-uint8 mask8bit = 0x7f;		// TO be used for masking 8 bit characters (XMODEM related)
+uint8 mask8bit;				// TO be used for masking 8 bit characters (XMODEM related)
 							// If set to 0x7f, RunCPM masks the 8th bit of characters sent
 							// to the console. This is the standard CP/M behavior.
 							// If set to 0xff, RunCPM passes 8 bit characters. This is
@@ -31,27 +25,12 @@ uint8 _getchNB(void)		// Gets a character, non-blocking, no echo
 	return(_kbhit() ? _getch() : 0x00);
 }
 */
-void _putcon(uint8 ch)		// Puts a character
-{
-	_putch(ch & mask8bit);
-}
+void _putcon(uint8 ch);		// Puts a character
 
-void _puts(const char *str)	// Puts a \0 terminated string
-{
-	while (*str)
-		_putcon(*(str++));
-}
+void _puts(const char *str);	// Puts a \0 terminated string
 
-void _puthex8(uint8 c)		// Puts a HH hex string
-{
-	_putcon(tohex(c >> 4));
-	_putcon(tohex(c & 0x0f));
-}
+void _puthex8(uint8 c);		// Puts a HH hex string
 
-void _puthex16(uint16 w)	// puts a HHHH hex string
-{
-	_puthex8(w >> 8);
-	_puthex8(w & 0x00ff);
-}
+void _puthex16(uint16 w);	// puts a HHHH hex string
 
 #endif
--- main.c
+++ main.c
@@ -2,5 +2,6 @@
 
 int main(int argc, char *argv[])
 {
+	Z80reset();
     Z80run();
 }

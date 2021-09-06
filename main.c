80df33fc873e8eb374c1fcb33458a1b7220d20db
Vlad Shurupov <vlad.shurupov@gmail.com>
2021-09-07 00:56:33 +1000

    Added comments to main.c

--- main.c
+++ main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include "z80.h"
 
+/*
+ * The intercept function is invoked on each and every CPU instruction. It
+ * quits the simulation when the PC register reaches address #0005 by changing
+ * the virtual CPU status.
+ */
 static void intercept(void *ctx)
 {
     z80 *cpu = ctx;
@@ -12,6 +17,11 @@ static void intercept(void *ctx)
 
 int main(int argc, char *argv[])
 {
+    /*
+     * Instantiate the CPU, activate the debug mode, set the intercept
+     * function and run the virtual CPU until the termination criteria
+     * set by the intercept function, if any, are met.
+     */
     z80 *cpu = z80_new();
     cpu->debug = 1;
     z80_set_intercept(cpu, cpu, intercept);
